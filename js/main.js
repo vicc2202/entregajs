47a5ada678cd368ffbc1b2aa8e9852239de9f4df
victoriarodriguez <victoriamaluenda1@outlook.es>
2024-03-06 22:03:57 -0300

    Segundo commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+let ganaste;
+ganaste = "Felicidades, sumaste 1 punto :)"
+let perdiste;
+perdiste = "Lo lamento, perdiste 1 punto :("
+
+let comienzoDeJuego = prompt ("¿Desea comenzar a jugar? \nSi \nNo")
+if (comienzoDeJuego == "Si") {
+    alert ("¡A jugar!")
+} else {
+    alert ("Vos te lo perdes")
+}
+
+let pregunta1 = prompt ("¿Con la hija de qué figura de la música se casó Michael Jackson? \nFrank Sinatra \nElvis Presley")
+if (pregunta1 == "Elvis Presley") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta2 = prompt ("¿Qué número representa `el enamorado´ en los sueños de la quiniela argentina? \n93 \n39")
+if (pregunta2 == "93") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta3 = prompt ("¿En qué parte de su cuerpo tenía Diego Armando Maradona un tatuaje de Fidel Castro? \nPierna izquierda \nPecho")
+if (pregunta3 == "Pierna izquierda") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta4 = prompt ("Según la tradición, ¿cuál de estos objetos no se debe regalar nunca? \nUn cuchillo \nUna copa")
+if (pregunta4  == "Un cuchillo") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta5 = prompt ("¿Cuántos hermanos tuvo el presidente Domingo Faustino Sarmiento? \n8 \n14")
+if (pregunta4  == "14") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta6 = prompt ("Según la tradición, ¿qué va a ocurrirle a una persona que es golpeada casualmente con el corcho tras destapar una botella? \nSe va a separar \nSe va a casar")
+if (pregunta4  == "Se va a casar") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta7 = prompt ("¿Frente a qué edificio histórico se encuentra el ´Monolito Kilómetro 0´, que mide las rutas nacionales que parten de Buenos Aires?\nCasa Rosada \nCongreso de la Nación")
+if (pregunta4  == "Congreso de la Nación") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta8 = prompt ("¿Cuál de estas famosas obras del siglo XX pertenece al pensador francés Michael Foucault? \nEl príncipe \nEl ser y el tiempo \nEl nacimiento de la tragedia \nVigilar y castigar")
+if (pregunta4  == "Vigilar y castigar") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta9 = prompt ("Según la versión tradicional del famoso cuento infantil, son dos los cerditos que escapan del lobo que derriba casas soplando. \nVerdadero \nFalso")
+if (pregunta4  == "Verdadero") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
+let pregunta10 = prompt ("¿Cuál de estas palabras no aparece en el diccionario de la RAE? \nHelicótero \nAlmóndiga \nMurciégalo \nGüisquiespuesta")
+if (pregunta4  == "Helicótero") {
+    alert (ganaste)
+} else {
+    alert (perdiste)
+}
+
